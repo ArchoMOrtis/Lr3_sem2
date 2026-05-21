53d01c8455636f8e4145779747ce6ff2cc4e4c16
Konstantin <gomilovskykostya@mail.ru>
2026-05-21 14:32:22 +0700

    task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -5,49 +5,57 @@
 using namespace std;
 
 void table() {
-    cout << "|   x   |    y    |" << endl;
-    cout << "-------------------" << endl;
+    cout << "|    x    |     y     |" << endl;
+    cout << "------------------------" << endl;
 }
 
 void printTable(float x, float y) {
-    cout << "|" << setw(6) << fixed << setprecision(1) << x << " |"
-         << setw(8) << fixed << setprecision(2) << y << " |" << endl;
+    cout << "|" << setw(8) << fixed << setprecision(1) << x << " |"
+         << setw(10) << fixed << setprecision(2) << y << " |" << endl;
 }
 
 void printEnd() {
-    cout << "-------------------" << endl;
+    cout << "------------------------" << endl;
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
     float x, y;
+    float dx = 0.5;
 
     cout << "Таблица значений функции:\n";
+
     printEnd();
     table();
 
-    // 1. Прямая: [-9; -3]
-    for (x = -9; x <= -3; x++) {
-        y = 0.5 * x + 1.5;
+    // 1. Прямая [-5; -2]
+    for (x = -5; x < -2; x += dx) {
+        y = -x - 2;
         printTable(x, y);
     }
 
-    // 2. Экспонента: [-3; 1]
-    for (; x <= 1; x++) {
-        y = pow(3, x);
+    // 2. Четверть окружности [-2; -1]
+    for (x = -2; x < -1; x += dx) {
+        y = sqrt(1 - pow(x + 1, 2));
         printTable(x, y);
     }
 
-    // 3. Полуокружность: [1; 5]
-    for (; x <= 5; x++) {
-        y = 3 - sqrt(4 - pow(x - 3, 2));
+    // 3. Горизонтальный участок [-1; 1]
+    for (x = -1; x < 1; x += dx) {
+        y = 1;
         printTable(x, y);
     }
 
-    // 4. Прямая: [5; 8]
-    for (; x <= 8; x++) {
-        y = -x + 8;
+    // 4. Прямая [1; 2]
+    for (x = 1; x < 2; x += dx) {
+        y = -2 * x + 3;
+        printTable(x, y);
+    }
+
+    // 5. Парабола [2; 6]
+    for (x = 2; x <= 6; x += dx) {
+        y = pow(x - 2, 2) / 8 - 1;
         printTable(x, y);
     }
 
